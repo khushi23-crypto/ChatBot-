97ac8a381746203d1bf0d5f0198fd467d5711ebe
Kajal Saini <sizukabts91@gmail.com>
2025-08-23 00:24:50 +0530

    Finally Complete Frontend Part

--- src/App.js
+++ src/App.js
@@ -99,7 +99,64 @@ function App() {
     })
   }
   return (
-    
+    <div className='p-[20px] w-full h-[100vh] flex items-center flex-col ' style={{ backgroundImage: `url(${bg})`, backgroundSize: "100% 100%" }}>
+      <h1 className='text-white font-bold text-[30px] mt-10'>Real Time Chat App</h1>
+
+      {
+      !isLogin ? (
+        <div className='w-[90%] max-w-[600px] h-[400px] p-[20px] shadow-2xl backdrop-blur bg-[#00000020] mt-[30px] rounded-xl flex flex-col items-center justify-center gap-[30px] border-2 border-gray-700'>
+          <h1 className='text-2xl font-semibold text-white'>Select User</h1>
+          <select className='px-[50px] py-[5px] rounded-xl bg-[#1f2525] text-white h-[6vh] flex justify-center'
+            onChange={(e) => setSelectedUser(e.target.value)}
+            value={selectedUser} >
+            <option value="Kajal">Kajal</option>
+            <option value="Friend">Friend</option>
+          </select>
+          <button className='p-[10px] bg-white font-semibold text-black rounded-lg cursor-pointer w-[100px]'
+            onClick={handleLogin}>Login</button>
+        </div>
+      ) : (
+        <div className='w-[90%] max-w-[800px] h-[600px] p-5 backdrop-blur shadow-2xl bg-[#00000020] mt-8 rounded-xl flex flex-col items-center gap-[30px] border-2 border-gray-700 overflow-auto'>
+          <h2 className='text-white  text-[20px]'>
+            {userInfo.userName} <span className='text-gray-400'>chatting with</span> {selectedUser === 'Kajal' ? 'Friend' : 'Kajal'}
+          </h2>
+          <div className='w-full h-[1px] bg-gray-800 '></div>
+          <div className='rounded-2xl w-full p-[20px] flex flex-col gap-[10px] items-center h-[400px] overflo-auto'>
+            {messages.map((msg, i) => {
+              const isOwnMessage = msg.senderUserID === userInfo.userID;
+              return (
+                <div key={i} className={`w-full flex ${isOwnMessage ? 'justify-end' : 'justify-start'}`}>
+                  <div className={`px-[20px] py-[10px] shadow-lg text-white ${isOwnMessage
+                    ? 'bg-[#0f1010] text-white rounded-br-none rounded-t-2xl rounded-bl-2xl'
+                    : 'bg-[#1c2124] rounded-bl-none rounded-t-2xl rounded-br-2xl'}text-white`}>
+                    <div>{msg.message}</div>
+                    <div className='text-[13px] text-white'>{formatTime(msg.timestamp)}
+
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+            <div ref={messageEndRef} />
+            <div className='w-full flex gap-[20px] items-center h-[100px] fixed bottom-0 px-[20px] justify-center'>
+              <input
+                type='text'
+                placeholder='Type a message...'
+                className='rounded-2xl bg-gray-700 outline-none text-white px-[20px] py-[10px] placeholder-white w-full'
+                onChange={(e) => setMessageText(e.target.value)}
+                value={messageText}
+              />
+              <button
+                className='p-[10px] bg-white text-black rounded-2xl w-[100px] font-semibold'
+                onClick={handleSendMessage}
+              >
+                Send
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
   );
 }
 
